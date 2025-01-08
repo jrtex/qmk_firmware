80bdd6b29ad40b9269f397b904df404c93ad76e5
jrtex <jordan31891@gmail.com>
2025-01-07 22:29:29 -0500

    Add symetric access to Kom2 layer

--- keyboards/crkbd/keymaps/zkomorebi/keymap.c
+++ keyboards/crkbd/keymaps/zkomorebi/keymap.c
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
+                                          KC_LGUI,   MO(3), _______,   MO(_KOM2), SHFTFUN, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
